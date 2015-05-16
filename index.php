4d0b4349d36a437d42a654fef4095f38aa696771
SrinivasP <srnvs0708@gmail.com>
2015-05-16 12:19:15 +0530

    Initial Commit - sass boilerplate template

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Beat my salary</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="_/css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="_/css/styles.css" media="screen">
+</head>
+<body>
+	<h1>Hello, world!</h1>
+
+	<script src="_/js/bootstrap.js"></script>
+	<script src="_/js/script.js"></script>
+</body>
+</html>
